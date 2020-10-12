9391a6a781e4a9deafef6dea4d7a6b519efc5482
phisaw <57394869+phisaw@users.noreply.github.com>
2020-10-12 13:51:49 +0200

    first commit

---
+++ TSI-calc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TSI_calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
